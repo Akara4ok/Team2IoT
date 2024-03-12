1b00615e1d7bfba12ed2c2b0acdf80ce576e82da
voonya <nikolaiev.i03@gmail.com>
2024-03-12 17:38:20 +0200

    add mqtt client

--- agent/src/config.py
+++ agent/src/config.py
@@ -7,3 +7,13 @@ def try_parse(type, value: str):
     except Exception:
         return None
 
+
+USER_ID = 0
+# MQTT config
+MQTT_BROKER_HOST = os.environ.get("MQTT_BROKER_HOST") or "mqtt"
+MQTT_BROKER_PORT = try_parse(int, os.environ.get("MQTT_BROKER_PORT")) or 1883
+MQTT_TOPIC = os.environ.get("MQTT_TOPIC") or "agent"
+
+# Delay for sending data to mqtt in seconds
+DELAY = try_parse(float, os.environ.get("DELAY")) or 1
+
